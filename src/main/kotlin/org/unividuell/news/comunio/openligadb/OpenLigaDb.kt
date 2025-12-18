6a70dcfc0a61f106c2053e0815292689799309ba
Clemens Hahn <clemens@couchbits.com>
2025-12-18 19:54:28 +0100

    determine the current match group

--- src/main/kotlin/org/unividuell/news/comunio/openligadb/OpenLigaDb.kt
+++ src/main/kotlin/org/unividuell/news/comunio/openligadb/OpenLigaDb.kt
@@ -2,14 +2,28 @@ package org.unividuell.news.comunio.openligadb
 
 import de.openligadb.api.MatchdataApi
 import de.openligadb.model.Match
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
+import java.time.Instant
+import java.time.LocalDate
 import java.time.ZoneId
+import java.time.temporal.ChronoUnit
 
 @Component
 class OpenLigaDb {
 
-    private val client = RestClient.builder().baseUrl("https://api.openligadb.de/").build()
+    private val logger = KotlinLogging.logger {  }
+
+    private val zoneBerlin = ZoneId.of("Europe/Berlin")
+
+    private val client = RestClient.builder()
+        .baseUrl("https://api.openligadb.de/")
+        .requestInterceptor { request, bytes, execution ->
+            logger.debug { ">>> ${request.method} ${request.uri}" }
+            execution.execute(request, bytes)
+        }
+        .build()
 
     val matches: List<Match> = getMatchDataApi()
 
@@ -18,11 +32,35 @@ class OpenLigaDb {
     val kickOffsByMatchGroup = matches
         .groupBy { it.group?.groupOrderID }
         .mapValues { it.value.map { it.matchDateTimeUTC }.toSet() }
-        .mapValues { it.value.map { it?.atZoneSameInstant(ZoneId.of("Europe/Berlin")) } }
+        .mapValues { it.value.mapNotNull { it?.atZoneSameInstant(zoneBerlin) } }
+
+    val kickOffsGroupStartEnd = kickOffsByMatchGroup
+        .mapValues { Pair(it.value.minBy { it }, it.value.maxBy { it }) }
 
     fun getMatchDataApi(): List<Match> {
         return MatchdataApi(client)
             .getmatchdataLeagueShortcutLeagueSeasonGet(leagueShortcut = "bl1", leagueSeason = 2025)
+    }
+
+    data class MatchGroup(
+        val groupOrderId: Int,
+    )
+
+    fun currentMatchGroup(relativeTo: Instant): MatchGroup? {
+        return kickOffsGroupStartEnd
+            .mapValues {
+                // extend the range of the match group by -1 and +3 days (at start of day)
+                // caution: new end of match day n wins over new start of match day n+1!
+                // aka: we are iterating from day 1 to 34 and trying to find the first match
+                Pair(
+                    it.value.first.toOffsetDateTime().toLocalDate().minusDays(1).atStartOfDay(zoneBerlin).toInstant(),
+                    it.value.second.toOffsetDateTime().toLocalDate().plusDays(4).atStartOfDay(zoneBerlin).toInstant()
+                )
+            }
+            .entries
+            .firstOrNull { relativeTo >= it.value.first && relativeTo < it.value.second }
+            ?.also { logger.debug { "current group for ${relativeTo}: ${it.key} (${it.value})" } }
+            ?.key?.let { MatchGroup(groupOrderId = it) }
     }
 
 }
--- src/test/kotlin/org/unividuell/news/comunio/openligadb/OpenLigaDbTest.kt
+++ src/test/kotlin/org/unividuell/news/comunio/openligadb/OpenLigaDbTest.kt
@@ -1,20 +1,29 @@
 package org.unividuell.news.comunio.openligadb
 
 import io.kotest.matchers.maps.shouldHaveSize
+import io.kotest.matchers.shouldBe
 import io.micrometer.common.annotation.ValueExpressionResolver
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.ai.model.openaisdk.autoconfigure.OpenAiSdkChatAutoConfiguration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.modulith.test.ApplicationModuleTest
+import org.springframework.test.context.TestPropertySource
+import java.time.OffsetDateTime
 import java.time.ZoneId
+import java.time.ZonedDateTime
 import kotlin.collections.mapValues
 import kotlin.time.Duration.Companion.minutes
 
 @ApplicationModuleTest
 @EnableAutoConfiguration(exclude=[ OpenAiSdkChatAutoConfiguration::class ])
+@TestPropertySource(properties = ["logging.level.org.unividuell.news.comunio.openligadb=DEBUG"])
 class OpenLigaDbTest {
 
     @Autowired
@@ -27,5 +36,48 @@ class OpenLigaDbTest {
         // assert
         println(actual.map { "${it.key.toString().padStart(2)} -> ${it.value}" }.joinToString("\n"))
         actual shouldHaveSize 34
+    }
+
+    @Test
+    fun `it should provide the start and end instant of a group`() {
+        // act
+        val actual = sut.kickOffsGroupStartEnd
+        // assert
+        actual.getValue(15).run {
+            first.toOffsetDateTime() shouldBe OffsetDateTime.parse("2025-12-19T20:30+01:00")
+            second.toOffsetDateTime() shouldBe OffsetDateTime.parse("2025-12-21T17:30+01:00")
+        }
+    }
+
+    @Nested
+    inner class CurrentMatchGroup {
+
+        @ParameterizedTest
+        @CsvSource(value = [
+            "2025-12-16T00:00+01:00, 14", // -3d
+            "2025-12-16T23:59+01:00, 14", // -3d
+            "2025-12-17T00:00+01:00, 14", // -2d
+            "2025-12-17T23:59+01:00, 14", // -2d
+
+            "2025-12-18T10:32+01:00, 15", // -1d
+            "2025-12-19T20:29+01:00, 15", // -0d
+            "2025-12-19T20:30+01:00, 15", // first match at 15
+            "2025-12-21T17:30+01:00, 15", // last match at 15
+            "2025-12-21T17:31+01:00, 15", // +0d
+            "2025-12-22T17:30+01:00, 15", // +1d
+            "2025-12-23T23:59+01:00, 15", // +2d
+            "2025-12-24T00:00+01:00, 15", // +3d
+            "2025-12-24T23:59+01:00, 15", // +3d
+
+            "2025-12-25T00:00+01:00, null", // +4d
+        ], nullValues = ["null"])
+        fun `before, inside, and after match group 15`(offsetDateTime: String, expected: String?) {
+            // arrange
+            val relativeTo = OffsetDateTime.parse(offsetDateTime)
+            // act
+            val actual = sut.currentMatchGroup(relativeTo = relativeTo.toInstant())
+            // assert
+            actual?.groupOrderId shouldBe expected?.toInt()
+        }
     }
 }
