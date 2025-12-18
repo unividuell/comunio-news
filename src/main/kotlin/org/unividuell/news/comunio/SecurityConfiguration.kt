6ebfa17a52049f61d953c84cf4461130488cc457
Clemens Hahn <clemens@couchbits.com>
2025-12-18 12:41:38 +0100

    permit all

---
+++ src/main/kotlin/org/unividuell/news/comunio/SecurityConfiguration.kt
@@ -0,0 +1,19 @@
+package org.unividuell.news.comunio
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .authorizeHttpRequests { authorizeRequests -> authorizeRequests.anyRequest().permitAll()}
+            .build()
+    }
+}
