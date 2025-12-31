4824964f3a679135c0b31acb1b1771b10cb273ba
Clemens Hahn <clemens@couchbits.com>
2025-12-31 11:13:57 +0100

    verify the modularity and generate module documentation

--- src/test/kotlin/org/unividuell/news/comunio/ComunioNewsApplicationTests.kt
+++ src/test/kotlin/org/unividuell/news/comunio/ComunioNewsApplicationTests.kt
@@ -3,6 +3,8 @@ package org.unividuell.news.comunio
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.modulith.core.ApplicationModules
+import org.springframework.modulith.docs.Documenter
 import org.springframework.test.context.TestPropertySource
 
 @Import(TestcontainersConfiguration::class)
@@ -13,8 +15,23 @@ import org.springframework.test.context.TestPropertySource
 @SpringBootTest
 class ComunioNewsApplicationTests {
 
+    private val modules = ApplicationModules.of(ComunioNewsApplication::class.java)
+
     @Test
     fun contextLoads() {
+    }
+
+    @Test
+    fun modules() {
+        modules.verify()
+        modules.forEach { println(it) }
+    }
+
+    @Test
+    fun moduleDocumentation() {
+        Documenter(modules)
+            .writeModulesAsPlantUml()
+            .writeIndividualModulesAsPlantUml()
     }
 
 }
